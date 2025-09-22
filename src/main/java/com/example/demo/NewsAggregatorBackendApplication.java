dd7429d9170985636d60fda356e4f06147638dea
AnkithChowdary <ankithc29@gmail.com>
2025-09-23 00:08:46 +0530

    feat: configure project for WAR packaging and external Tomcat support

--- src/main/java/com/example/demo/NewsAggregatorBackendApplication.java
+++ src/main/java/com/example/demo/NewsAggregatorBackendApplication.java
@@ -2,12 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;  // 🔑 Import this
 
 @SpringBootApplication
-public class NewsAggregatorBackendApplication {
+public class NewsAggregatorBackendApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(NewsAggregatorBackendApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(NewsAggregatorBackendApplication.class, args);
     }
-
 }
